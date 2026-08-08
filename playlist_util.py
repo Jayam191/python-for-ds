d66c62d21986d334101881db856ba9dbb5ac9989
jayam191 <pjayam19@gmail.com>
2026-08-08 17:42:27 +0530

    session 14,15,16,17,18,19,20,21 done

---
+++ playlist_util.py
@@ -0,0 +1,7 @@
+
+def playlist_songs(playlist,song):
+    playlist.append(song)
+
+playlist = ["blue eye","daspas shito","party shati"]
+playlist_songs(playlist,"perfect")
+print(playlist)           
